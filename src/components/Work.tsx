1f921f7ca541253095b464f6f53ad81da8081164
lucasfilipess <lucasfilipedasilvasouza@gmail.com>
2021-11-11 20:12:43 -0300

    fix button label

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -133,7 +133,7 @@ const Work = ({ portfolio }: Props) => {
                             className="inline-flex bg-gradient-to-r from-teal-500 to-cyan-600 bg-origin-border px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white hover:from-teal-600 hover:to-cyan-700"
                             rel="noreferrer"
                           >
-                            View project
+                            {landing.viewButton}
                           </a>
                         </div>
                       )}
